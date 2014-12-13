e2435061263b65c5590ec04901598e53d42aa39a
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:53:38 -0800

    user has many tours and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true
+  has_many :tours
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :phone }
+  it { should have_many :tours }
 
   it "should have valid factory" do
     expect(FactoryGirl.build(:user)).to be_valid
