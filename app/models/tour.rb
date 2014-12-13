a573e6eeeeab816f7b10ff2f1f8093897d6e7f90
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:54:48 -0800

    tour should belong to user and test

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,7 @@
 class Tour < ActiveRecord::Base
 
   has_many :amenities
+  belongs_to :user
   validates :tour_date, presence: true
 
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,6 +1,7 @@
 describe Tour do
 
-  it { should have_many :amenities}
-  it { should validate_presence_of :tour_date}
+  it { should have_many :amenities }
+  it { should validate_presence_of :tour_date }
+  it { should belong_to :user }
 
 end
