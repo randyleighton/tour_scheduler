a0ec2f948cde5c92de7d4498e907b80d7a3670ac
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:28:26 -0800

    user validate presence of email and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+  validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true
