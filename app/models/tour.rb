81de4c10e853b348e7ec99a9517ee538bb69d8f2
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:22:09 -0800

    user should validate presence of last name and test

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,5 +3,4 @@ class Tour < ActiveRecord::Base
   has_many :amenities
   validates :tour_date, presence: true
 
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 describe User do
 
   it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
 
 end
