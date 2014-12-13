6770038b241aa459f4efe5ae8c01f4233ce091c6
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:48:32 -0800

    add index new strong params to tour, need user id

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,3 +1,25 @@
 class ToursController < ApplicationController
 
+  def index
+    @tours = Tour.all
+  end
+
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+
+  end
+
+
+  def show
+
+  end
+
+private
+  def tour_params
+    params.require(:tour).permit(:description, :user_id)
+  end
+
 end
