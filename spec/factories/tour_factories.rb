3b8fb78f9866c1502f91f8da63e21a4b2878f14f
Randy Leighton <randyleighton@yahoo.com>
2014-12-12 16:37:51 -0800

    factory girl build user successful test

--- spec/factories/tour_factories.rb
+++ spec/factories/tour_factories.rb
@@ -1,10 +1,18 @@
 FactoryGirl.define do
 
   factory :user do
-    first_name "Jake"
+    first_name "John"
     last_name "Jones"
     phone "408-123-4567"
     email "jake@yahoo.com"
+  end
+
+  factory :amenity do
+    description "pool"
+  end
+
+  factory :tour do
+    tour_date "12/11/2014"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,9 @@ describe User do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :phone }
-  
+
+  it "should have valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
 end
